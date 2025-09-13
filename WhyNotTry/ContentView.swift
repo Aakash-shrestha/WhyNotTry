b9f80ef341eb33ead60ded9495f1421b9bafd2e2
Aakash-shrestha <akash.stha444@icloud.com>
2025-09-13 19:45:48 +0545

    added random button

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,16 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    var activities =  ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    @State var selected = "Baseball" //mutable element
+    
     var body: some View{
-        Circle()
-            .fill(.blue)
-            .padding()
-            .overlay(
-                Image(systemName: "figure.archery")
-                .font(.system(size:144))
-                .foregroundColor(.white)
-            )
-    }
+        VStack{
+            Text("Why Not Try...")
+                .font(.largeTitle.bold())
+            
+            VStack{
+                Circle()
+                    .fill(.blue)
+                    .padding()
+                    .overlay(
+                        Image(systemName: "figure.\(selected.lowercased())")
+                        .font(.system(size:144))
+                        .foregroundColor(.white)
+                    )
+                
+                Text("\(selected)!")
+                    .font(.title)
+            }
+            
+            Button("Try Again"){
+                selected = activities.randomElement() ?? "Archery"
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        }
 }
 
 #Preview {
