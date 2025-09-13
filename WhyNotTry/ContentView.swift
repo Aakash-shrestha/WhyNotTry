3300294ab3100fbfd3aea929b90fb2aa02aa21d4
Aakash-shrestha <akash.stha444@icloud.com>
2025-09-13 19:25:59 +0545

    added archery man

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    var body: some View{
+        Circle()
+            .fill(.blue)
+            .padding()
+            .overlay(
+                Image(systemName: "figure.archery")
+                .font(.system(size:144))
+                .foregroundColor(.white)
+            )
     }
 }
 
